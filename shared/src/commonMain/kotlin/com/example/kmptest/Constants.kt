8742f66c1ddfc7762f6cd19e9cbe8af52e16708e
frybitsinc <frybitsinc@gmail.com>
2025-09-05 17:14:15 +0900

    first commit

---
+++ shared/src/commonMain/kotlin/com/example/kmptest/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.kmptest
+
+const val SERVER_PORT = 8080
---
+++ shared/src/jvmMain/kotlin/com/example/kmptest/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.example.kmptest
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
